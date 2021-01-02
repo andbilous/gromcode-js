678ace700c5d90ee4a995b69eb8b1907f6f69e69
Bilous Andrii <andbilous@gmail.com>
2021-01-02 13:51:33 +0200

    tests

---
+++ calculator.js
@@ -0,0 +1,7 @@
+export const getSquaredArray = arr => arr.map(item => item * item)
+
+export const getOddNumbers = arr => arr.filter(item => item % 2 === 1);
+
+ const getSum = (num1, num2) => num1 + num2;
+
+ export default getSum;
---
+++ calculator.test.js
@@ -0,0 +1,23 @@
+import getSum, {
+  getSquaredArray,
+  getOddNumbers
+} from './calculator';
+
+
+
+
+it('getSquaredArray test', () => {
+  const result = getSquaredArray([1, 2, 3, 4]);
+  expect(result).toEqual([1, 4, 9, 16]);
+})
+
+
+it('getOddNumbers test', () => {
+  const result = getOddNumbers([1, 2, 3, 4]);
+  expect(result).toEqual([1, 3]);
+})
+
+it('getSum test', () => {
+  const result = getSum(1, 2);
+  expect(result).toEqual(3)
+})
