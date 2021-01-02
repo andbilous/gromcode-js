14fb5f0396078729169faffb85e5a4eae2a5257c
Bilous Andrii <andbilous@gmail.com>
2021-01-02 17:43:17 +0200

    sf

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
-/* eslint-disable no-restricted-syntax */
+let message = "Just learn it!";
 
-export function reverseArray(arr) {
-  if (!Array.isArray(arr)) {
-    return null;
-  }
-  const res = [...arr];
-  return res.reverse();
+export function sendMessage(name) {
+  const sender = "Gromcode";
+  console.log(`${name}, ${message} Your ${sender}`);
 }
+export function setMessage(text) {
+  message = text;
+}
+setMessage("Good job!");
+sendMessage("Ann");
 
-export const withdraw = (clients, balances, client, amount) => {
-  const index = clients.indexOf(client);
-  if (balances[index] - amount < 0) {
-    return -1;
-  }
-  return balances[index] - amount;
-};
-
-export const getAdults = (obj) => {
-  if (typeof obj !== "object") {
-    return null;
-  }
-  const res = {};
-  for (const key in obj) {
-    if (obj[key] >= 18) {
-      res[key] = obj[key];
-    }
-  }
-  return res;
-};
+setMessage("Good job2!");
+sendMessage("Ann");
