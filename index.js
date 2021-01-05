381866d455b35b487960c07e8d40fe2a8a9bced5
Bilous Andrii <andbilous@gmail.com>
2021-01-05 16:44:14 +0200

    fix

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-let message = "Just learn it!";
+/* eslint-disable no-restricted-syntax */
 
-export function sendMessage(name) {
-  const sender = "Gromcode";
-  console.log(`${name}, ${message} Your ${sender}`);
-}
-export function setMessage(text) {
-  message = text;
-}
-setMessage("Good job!");
-sendMessage("Ann");
+export const reverseArray = (arr) => {
+  if (!Array.isArray(arr)) {
+    return null;
+  }
+  const res = [...arr];
+  return res.reverse();
+};
 
-setMessage("Good job2!");
-sendMessage("Ann");
+export const withdraw = (clients, balances, client, amount) => {
+  const index = clients.indexOf(client);
+  if (balances[index] - amount < 0) {
+    return -1;
+  }
+  return balances[index] - amount;
+};
+
+export const getAdults = (obj) => {
+  if (typeof obj !== "object") {
+    return null;
+  }
+  const res = {};
+  for (const key in obj) {
+    if (obj[key] >= 18) {
+      res[key] = obj[key];
+    }
+  }
+  return res;
+};
--- index.test.js
+++ index.test.js
@@ -1,46 +1,46 @@
 import { reverseArray, withdraw, getAdults } from "./index";
 
-it("reverseArray null test", () => {
+it("should return null", () => {
   const res = reverseArray(4);
   expect(res).toEqual(null);
 });
 
-it("reverseArray positive test", () => {
+it("should get reversed numbers", () => {
   const res = reverseArray([1, 2, 3]);
   expect(res).toEqual([3, 2, 1]);
 });
 
-it("reverseArray negative test", () => {
+it("should not get same numbers", () => {
   const res = reverseArray([1, 2, 3]);
   expect(res).not.toEqual([1, 2, 3]);
 });
 
-it("withdraw negative balance test", () => {
+it("should get -1", () => {
   const res = withdraw(["Ann", "John", "User"], [1400, 87, -6], "User", 10);
   expect(res).toEqual(-1);
 });
 
-it("withdraw positive test", () => {
+it("should get positive number", () => {
   const res = withdraw(["Ann", "John", "User"], [1400, 87, -6], "John", 50);
   expect(res).toEqual(37);
 });
 
-it("withdraw negative test", () => {
+it("should not get incorrect value", () => {
   const res = withdraw(["Ann", "John", "User"], [1400, 50, -6], "John", 40);
   expect(res).not.toEqual(11);
 });
 
-it("getAdults null test", () => {
+it("should get null", () => {
   const res = getAdults(4);
   expect(res).toEqual(null);
 });
 
-it("getAdults positive test", () => {
+it("should get adults object", () => {
   const res = getAdults({ A: 19, B: 3, K: 18 });
   expect(res).toEqual({ A: 19, K: 18 });
 });
 
-it("getAdults negative test", () => {
+it("should not get incorrect adults object", () => {
   const res = getAdults({ A: 12, K: 18, B: 56 });
   expect(res).not.toEqual({ A: 12, B: 56 });
 });
