42996f90413314f0a0345b29a281595ade001e6c
Bilous Andrii <andbilous@gmail.com>
2021-01-13 16:10:16 +0200

    g

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -3,28 +3,10 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.getGoal = exports.getPlans = exports.getDescription = exports.getTitle = void 0;
+exports.setTitle = void 0;
 
-var getTitle = function getTitle() {
-  return document.querySelector('.title').textContent;
+var setTitle = function setTitle(text) {
+  document.querySelector('.title').textContent = text;
 };
 
-exports.getTitle = getTitle;
-
-var getDescription = function getDescription() {
-  return document.querySelector('.about').innerText;
-};
-
-exports.getDescription = getDescription;
-
-var getPlans = function getPlans() {
-  return document.querySelector('.plans').innerHTML;
-};
-
-exports.getPlans = getPlans;
-
-var getGoal = function getGoal() {
-  return document.querySelector('.goal').outerHTML;
-};
-
-exports.getGoal = getGoal;
+exports.setTitle = setTitle;
