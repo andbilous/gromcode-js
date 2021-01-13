90c04dd5d9dc457a6a7340a2555834c644aa4988
Bilous Andrii <andbilous@gmail.com>
2021-01-13 15:30:21 +0200

    v

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -1,52 +1,22 @@
 "use strict";
 
-function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getInputElement = exports.getTitleElement = void 0;
 
-function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
-
-function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
-
-function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
-
-var tasks = [{
-  text: "Buy milk",
-  done: false
-}, {
-  text: "Pick up Tom from airport",
-  done: false
-}, {
-  text: "Visit party",
-  done: false
-}, {
-  text: "Visit doctor",
-  done: true
-}, {
-  text: "Buy meat",
-  done: true
-}];
-
-var renderListItems = function renderListItems(listItems) {
-  var listElem = document.querySelector(".list");
-  var listItemsElems = listItems.sort(function (a, b) {
-    return a.done - b.done;
-  }).map(function (_ref) {
-    var text = _ref.text,
-        done = _ref.done;
-    var listItemElem = document.createElement("li");
-    listItemElem.classList.add("list__item");
-
-    if (done) {
-      listItemElem.classList.add("list__item_done");
-    }
-
-    var checkboxElem = document.createElement("input");
-    checkboxElem.setAttribute("type", "checkbox");
-    checkboxElem.checked = done;
-    checkboxElem.classList.add("list__item-checkbox");
-    listItemElem.append(checkboxElem, text);
-    return listItemElem;
-  });
-  listElem.append.apply(listElem, _toConsumableArray(listItemsElems));
+var getTitleElement = function getTitleElement() {
+  var titleElem = document.querySelector('.title');
+  console.dir(titleElem);
+  return titleElem;
 };
 
-renderListItems(tasks);
+exports.getTitleElement = getTitleElement;
+
+var getInputElement = function getInputElement() {
+  var inputElem = document.querySelector('input[type="text"]');
+  console.dir(inputElem);
+  return inputElem;
+};
+
+exports.getInputElement = getInputElement;
