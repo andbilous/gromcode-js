ff05cc5300950eefa6a0f3310ab06b0cda32f40a
Bilous Andrii <andbilous@gmail.com>
2021-01-14 18:46:39 +0200

    sd

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -3,15 +3,41 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.squaredNumbers = void 0;
+exports.saveCalls = void 0;
 
-var squaredNumbers = function squaredNumbers() {
-  Array.from(document.querySelectorAll(".number")).map(function (elem) {
-    var currentValue = elem.getAttribute("data-number");
-    elem.setAttribute("data-squared-number", currentValue * currentValue);
-    return elem;
-  });
+var saveCalls = function saveCalls(f) {
+  var calls = [];
+
+  function withMemory() {
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    calls.push(args);
+    return f.apply(this, args);
+  }
+
+  return withMemory;
 };
 
-exports.squaredNumbers = squaredNumbers;
-squaredNumbers();
+exports.saveCalls = saveCalls;
+
+function test(a, b) {
+  return Math.sqrt(a * a + b * b);
+}
+
+var testa = saveCalls(test);
+testa(4, 2);
+testa(9, 1);
+console.log(1, testa.calls);
+var user = {
+  name: "John",
+  sayHi: function sayHi() {
+    return this.name;
+  }
+};
+var methodWithMemory = saveCalls(user.sayHi);
+console.log(2, methodWithMemory.apply({
+  name: "Tom"
+}));
+console.log(3, methodWithMemory.calls);
--- index.js
+++ index.js
@@ -1,9 +1,41 @@
-export const squaredNumbers = () => {
-  Array.from(document.querySelectorAll(".number")).map((elem) => {
-    const currentValue = elem.getAttribute("data-number");
-    elem.setAttribute("data-squared-number", currentValue * currentValue);
-    return elem;
-  });
+const clearBtn = document.querySelector(".clear-btn");
+const removeHandlersBtn = document.querySelector(".remove-handlers-btn");
+const attachHandlersBtn = document.querySelector(".attach-handlers-btn");
+const divElem = document.querySelector(".rect_div");
+const pElem = document.querySelector(".rect_p");
+const spanElem = document.querySelector(".rect_span");
+const eventsList = document.querySelector(".events-list");
+
+const logTarget = (text, color) => {
+  eventsList.innerHTML += `<span style="color: ${color}; margin-left: 8px">${text}</span>`;
 };
 
-squaredNumbers();
+const logGreenDiv = logTarget.bind(null, "DIV", "green");
+const logGreenP = logTarget.bind(null, "P", "green");
+const logGreenSpan = logTarget.bind(null, "SPAN", "green");
+
+const attachListeners = () => {
+  divElem.addEventListener("click", logGreenDiv);
+  pElem.addEventListener("click", logGreenP);
+  spanElem.addEventListener("click", logGreenSpan);
+  divElem.addEventListener("click", logGreenDiv, true);
+  pElem.addEventListener("click", logGreenP, true);
+  spanElem.addEventListener("click", logGreenSpan, true);
+};
+
+const removeListeners = () => {
+  divElem.removeEventListener("click", logGreenDiv);
+  pElem.removeEventListener("click", logGreenP);
+  spanElem.removeEventListener("click", logGreenSpan);
+  divElem.removeEventListener("click", logGreenDiv, true);
+  pElem.removeEventListener("click", logGreenP, true);
+  spanElem.removeEventListener("click", logGreenSpan, true);
+};
+
+clearBtn.addEventListener("click", () => {
+  eventsList.innerHTML = "";
+});
+attachHandlersBtn.addEventListener("click", attachListeners);
+removeHandlersBtn.addEventListener("click", removeListeners);
+
+attachListeners();
