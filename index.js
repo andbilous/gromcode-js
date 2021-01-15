7effbd81aa3ef0098ad195d5cd0dd62b8906b11f
Bilous Andrii <andbilous@gmail.com>
2021-01-15 23:06:57 +0200

    dg

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-/* eslint-disable no-param-reassign */
 const listElem = document.querySelector(".list");
 const inputElem = document.querySelector(".task-input");
 const createBtn = document.querySelector(".create-task-btn");
-let newTask;
 
 const tasks = [
-  { id: "1", text: "Buy milk", done: false },
-  { id: "2", text: "Pick up Tom from airport", done: false },
-  { id: "3", text: "Visit party", done: false },
-  { id: "4", text: "Visit doctor", done: true },
-  { id: "5", text: "Buy meat", done: true },
+  { text: "Buy milk", done: false },
+  { text: "Pick up Tom from airport", done: false },
+  { text: "Visit party", done: false },
+  { text: "Visit doctor", done: true },
+  { text: "Buy meat", done: true },
 ];
 
 const generateId = () => Math.floor(Math.random() * 10000).toString();
 
 const renderTasks = (tasksList) => {
-  console.log(tasks);
   listElem.innerHTML = "";
   const tasksElems = tasksList
     .sort((a, b) => a.done - b.done)
-    .map(({ text, done, id }) => {
+    .map(({ text, done }, index) => {
       const listItemElem = document.createElement("li");
       listItemElem.classList.add("list__item");
       const checkbox = document.createElement("input");
       checkbox.setAttribute("type", "checkbox");
       checkbox.checked = done;
-      checkbox.setAttribute("data-id", id);
+      checkbox.setAttribute("data-id", index);
       checkbox.classList.add("list__item-checkbox");
       if (done) {
         listItemElem.classList.add("list__item_done");
@@ -36,32 +33,30 @@ const renderTasks = (tasksList) => {
     });
 
   listElem.append(...tasksElems);
-  console.log(tasksElems[1]);
 };
 
-inputElem.addEventListener("change", (e) => {
-  newTask = { id: generateId(), text: e.target.value, done: false };
-});
-
-listElem.addEventListener("click", (e) => {
-  const { id } = e.target.dataset;
-  tasks.map((item) => {
-    if (item.id === id) {
-      item.done = !item.done;
-      return item;
-    }
-    return item;
-  });
-  renderTasks(tasks);
-});
-
-createBtn.addEventListener("click", () => {
-  if (newTask) {
-    tasks.push(newTask);
-    newTask = undefined;
-    renderTasks(tasks);
-    inputElem.value = "";
+const addTask = () => {
+  if (!inputElem.value) {
+    return;
   }
-});
+  const newTask = {
+    text: inputElem.value,
+    done: false,
+  };
+  tasks.push(newTask);
+  renderTasks(tasks);
+};
+
+const toogleTask = (e) => {
+  if (e.target.classList.contains("list__item-checkbox")) {
+    const { id } = e.target.dataset;
+    tasks[id].done = e.target.checked;
+    renderTasks(tasks);
+  }
+};
+
+listElem.addEventListener("click", toogleTask);
+
+createBtn.addEventListener("click", addTask);
 
 renderTasks(tasks);
